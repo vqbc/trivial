4f40884827506c46c3e0ac9a7255dc337f74d942
Andrew Chang <andrewggchang@gmail.com>
2021-02-06 17:36:20 -0800

    Releasing as copyleft

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Trivial
 
 This website provides tools for more efficiently getting and **working with problems** on the [Art of Problem Solving Wiki](https://artofproblemsolving.com/wiki/), either as a **single problem or a practice batch**, with **solutions shown** in a separate section. Numerous **filtering & display options** are supported, and **custom problem batches** or **individual problems & tests** can also be displayed. Attractive **article display** is also supported, with full **search & history** functionality. All of this can be viewed in the browser with a convenient layout or **printed out**.
+
+## Maintenance
+
+The only parts of this project that will need regular manual updating are the [allpages.json](data/allpages.json) and [allproblems.json](data/allproblems.json) page lists for the wiki. To download updated copies that can then be committed, run [downloadlists.js](downloadlists.js) in the browser console.
+
+## License
+
+This project is copyrighted by Andrew Chang and licensed under the terms of the [Mozilla Public License v2.0](https://www.mozilla.org/en-US/MPL/2.0/).
---
+++ downloadlists.js
@@ -0,0 +1,81 @@
+let allPages = [];
+let allProblems = [];
+
+function downloadObject(obj, filename) {
+  obj = JSON.stringify(obj, undefined, 4);
+
+  var blob = new Blob([obj], { type: "text/json" }),
+    a = document.createElement("a");
+
+  a.download = filename;
+  a.href = window.URL.createObjectURL(blob);
+  a.dataset.downloadurl = ["text/json", a.download, a.href].join(":");
+  a.click();
+}
+
+let validProblem = (problem) =>
+  problem.includes("Problems/Problem") &&
+  problem.match(/^\d{4}/) &&
+  problem.match(/\d+$/);
+
+let computeTest = (problem) =>
+  problem
+    .match(/(\d{4} )(.*)( Problems)/)[2]
+    .replace(/AMC ((?:10)|(?:12))[AB]/, "AMC $1")
+    .replace(/AIME I+/, "AIME")
+    .replace(/AJHSME/, "AMC 8");
+let computeYear = (problem) => problem.match(/^\d{4}/)[0];
+let computeNumber = (problem) => problem.match(/\d+$/)[0];
+
+let sortProblems = (problems) => {
+  let sorted = problems.sort((a, b) => {
+    switch (Math.sign(computeYear(a) - computeYear(b))) {
+      case -1:
+        return -1;
+      case 1:
+        return 1;
+      case 0:
+        switch (computeTest(a).localeCompare(computeTest(b))) {
+          case -1:
+            return -1;
+          case 1:
+            return 1;
+          case 0:
+            return Math.sign(computeNumber(a) - computeNumber(b));
+        }
+    }
+  });
+  return sorted;
+};
+
+console.log("Preloading all wiki pages, allow around 15 seconds...");
+let apiEndpoint = "https://artofproblemsolving.com/wiki/api.php";
+let params = `action=query&list=allpages&aplimit=max&format=json`;
+let paramsContinue;
+
+let response = await fetch(`${apiEndpoint}?${params}&origin=*`);
+let json = await response.json();
+
+for (let page of json.query.allpages) {
+  if (page.title.charAt(0) !== "/") allPages.push(page.title);
+  if (validProblem(page.title)) allProblems.push(page.title);
+}
+
+while (json?.continue) {
+  console.log(`${Math.round((allPages.length / 13000) * 100)}% loaded...`);
+  paramsContinue = params + `&apcontinue=${json.continue.apcontinue}`;
+  response = await fetch(`${apiEndpoint}?${paramsContinue}&origin=*`);
+  json = await response.json();
+
+  for (let page of json.query.allpages) {
+    if (page.title.charAt(0) !== "/") allPages.push(page.title);
+    if (validProblem(page.title)) allProblems.push(page.title);
+  }
+}
+
+console.log(`Finished loading Special:AllPages (${allPages.length} pages).`);
+
+allProblems = sortProblems(allProblems);
+
+downloadObject(allPages, "allpages.json");
+downloadObject(allProblems, "allproblems.json");
