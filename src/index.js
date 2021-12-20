37f7662fdd42113ad66fedf48d0705adee5ff26d
Andrew Chang <andrewggchang@gmail.com>
2021-12-20 14:25:07 -0800

    Update page lists

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@
 (() => {
   let allPages = [];
   let allProblems = [];
-  $.getJSON("data/allpages.json?20211205", (json) => {
+  $.getJSON("data/allpages.json?20211220", (json) => {
     allPages = json;
   });
-  $.getJSON("data/allproblems.json?20211205", (json) => {
+  $.getJSON("data/allproblems.json?20211220", (json) => {
     allProblems = json;
   });
   let categoryPages = [];
