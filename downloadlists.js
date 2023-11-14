0928666efa5c7de45be1b38120f7ccab251c2602
Andi Chang <andrewggchang@gmail.com>
2023-11-14 09:33:27 -0800

    Fix preloading bug

--- downloadlists.js
+++ downloadlists.js
@@ -7,7 +7,7 @@ import fs from "fs";
 
   let validProblem = (problem) =>
     problem.includes("Problems/Problem") &&
-    problem.match(/(\d{4} )(.*)( Problems)/) &&
+    problem.match(/^(\d{4} )(.*)( Problems)/) &&
     problem.match(/\d+$/);
 
   let computeTest = (problem) =>
