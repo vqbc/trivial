d78445517964949920c10156314537238f6767cb
Andi Chang <andi.chang.vq@gmail.com>
2026-05-28 20:30:03 -0700

    Update downloadlists.js

--- downloadlists.js
+++ downloadlists.js
@@ -35,7 +35,7 @@ import fs from "fs";
     headers: {
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
     }
-  );
+  });
   let text = await response.text();
   console.log(text);
   let json = await response.json();
